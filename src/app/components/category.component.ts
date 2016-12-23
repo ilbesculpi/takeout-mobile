d318301908a4f80ef48b76a1be6a6e61375e7769
Ilbert Esculpi <ilbert.esculpi@gmail.com>
2016-12-23 08:30:31 -0400

    components

---
+++ src/app/components/category.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'category',
+    templateUrl: 'category.html'
+})
+export class FoodCategory {
+    title = 'Sandwich';
+    image = './assets/images/sandwich.jpg';
+}
